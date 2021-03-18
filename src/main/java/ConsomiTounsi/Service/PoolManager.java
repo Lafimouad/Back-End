1ab837d67b369bafbf56a8a580485675676986b8
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2021-03-18 21:40:55 +0100

    Update PoolManager.java

--- src/main/java/ConsomiTounsi/Service/PoolManager.java
+++ src/main/java/ConsomiTounsi/Service/PoolManager.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-
 @Service
 public class PoolManager implements PoolManagerInterface{
 	
