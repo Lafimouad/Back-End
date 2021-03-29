f0b92a1db3ba3a03e34ad05941a143ff2a943392
SarraMetoui <metoui.sarra@esprit.tn>
2021-03-29 21:37:19 +0100

    Create DelivController.java

---
+++ src/main/java/ControllerDevl/DelivController.java
@@ -0,0 +1,11 @@
+package ControllerDevl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Delivry")
+public class DelivController {
+	
+
+}
