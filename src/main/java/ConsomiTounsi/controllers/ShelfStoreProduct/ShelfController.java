8864ac95ab9a4cd1f763ebfc28c17e1bc5f3fee6
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2021-04-06 09:31:46 +0200

    Update ShelfController.java

--- src/main/java/ConsomiTounsi/controllers/ShelfStoreProduct/ShelfController.java
+++ src/main/java/ConsomiTounsi/controllers/ShelfStoreProduct/ShelfController.java
@@ -1,10 +1,30 @@
 package ConsomiTounsi.controllers.ShelfStoreProduct;
 
 
+import ConsomiTounsi.Service.ShelfManagerInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/accounts/user")
+@RequestMapping("/shelf")
 public class ShelfController {
+    @Autowired
+    ShelfManagerInterface shelfmanager ;
+
+
+    @GetMapping("/home")
+    public String Home(){
+        return ("<h1> WELCOME TO THE SHELF SERVICES </h1>");
+    }
+
+    @GetMapping("/findShelf/{type}")
+    public Claim FindClaim(@PathVariable("id") Long id){
+        return claimManagerI.FindClaim(id);
+
+    }
+
+
+
 }
