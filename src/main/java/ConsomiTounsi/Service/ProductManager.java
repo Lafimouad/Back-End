5e8f29a822dd9fb5e8eaf1e6e44bd8f92ca1dbfd
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2021-04-06 13:23:47 +0200

    jawi behy aal lekher ( mail )

--- src/main/java/ConsomiTounsi/Service/ProductManager.java
+++ src/main/java/ConsomiTounsi/Service/ProductManager.java
@@ -1,6 +1,9 @@
 package ConsomiTounsi.Service;
 
+import ConsomiTounsi.configuration.config.EmailSenderService;
+import ConsomiTounsi.entities.Admin;
 import ConsomiTounsi.entities.Product;
+import ConsomiTounsi.entities.Role;
 import ConsomiTounsi.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +16,8 @@ import java.util.Optional;
 @Service
     public class ProductManager implements ProductManagerInterface{
 
-	
+	@Autowired
+    AdminManagerInterface adminS;
 
 	@Autowired
     ProductRepository Pr;
@@ -81,11 +85,95 @@ import java.util.Optional;
         return Pr.AddQuantity(qn , id);
     }
 
+    @Autowired
+    EmailSenderService emailSenderService;
+
     @Override
     public int decrementProductQuantity(long id) {
         Product p = Pr.findById(id).orElse(new Product());
-        long qn = p.getQuantityProduct() - 1;
+        String product = p.getName_product();
+            long qn = p.getQuantityProduct() - 1;
+           if (qn<0){qn=0;}
+        List<Admin>  list = adminS.retrieveAllAdmin();
+        if ( qn < 10 )  {
+            for (Admin a : list) {
+                Role role = a.getRoleAdmin();
+                if (role.equals(Role.PRODUCTS_MANAGER)) {
+                    String name=a.getFirstNameUser();
+                    String subject = "Quantity almost done";
+                    emailSenderService.sendEmail(a.getEmailAddressUser(),body(name , product) ,subject );}
+            }
+        }
         return Pr.AddQuantity(qn , id);
+    }
+
+    public String body( String name , String product){
+        return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+                "\n" +
+                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+                "\n" +
+                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+                "        \n" +
+                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                "          <tbody><tr>\n" +
+                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td style=\"padding-left:10px\">\n" +
+                "                  \n" +
+                "                    </td>\n" +
+                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
+                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Consomi Tounsi #619</span>\n" +
+                "                    </td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "              </a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
+                "      <td>\n" +
+                "        \n" +
+                "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td bgcolor=\"#7C0A02\" width=\"100%\" height=\"10\"></td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+                "        \n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> the product " + product + "'s quantity is less than 10 items , it's about time to contact the supplier</p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\">" +
+                "  \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                "\n" +
+                "</div></div>";
     }
 
     @Override
