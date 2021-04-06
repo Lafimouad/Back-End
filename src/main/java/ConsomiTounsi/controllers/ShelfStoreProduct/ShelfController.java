376c587cb98425bc0409dc7cff243b84d9ec0a46
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2021-04-06 09:37:13 +0200

    Update ShelfController.java

--- src/main/java/ConsomiTounsi/controllers/ShelfStoreProduct/ShelfController.java
+++ src/main/java/ConsomiTounsi/controllers/ShelfStoreProduct/ShelfController.java
@@ -2,10 +2,11 @@ package ConsomiTounsi.controllers.ShelfStoreProduct;
 
 
 import ConsomiTounsi.Service.ShelfManagerInterface;
+import ConsomiTounsi.entities.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/shelf")
@@ -19,11 +20,46 @@ public class ShelfController {
         return ("<h1> WELCOME TO THE SHELF SERVICES </h1>");
     }
 
-    @GetMapping("/findShelf/{type}")
-    public Claim FindClaim(@PathVariable("id") Long id){
-        return claimManagerI.FindClaim(id);
+    @PostMapping("/add")
+    public void addShelf(@RequestBody Shelf s){ commentS.addComment(a);}
 
+    @GetMapping("/retrieve-all")
+    public List<Comment> getListShelfs(){ return commentS.retrieveAllComment(); }
+
+    @GetMapping("/retrieve-id")
+    public Comment getShelfById(@RequestParam("id") long id){
+        return commentS.FindComment(id);
     }
+
+    @DeleteMapping("remove-id")
+    public void removeShelfByID(@RequestParam("id")long id){
+        commentS.deleteComment(id);
+    }
+
+    @PutMapping("/update")
+    public Comment updateShelf(@RequestBody Comment a){
+        return commentS.updateComment(a);
+    }
+
+    /*@PutMapping("/likeComment-id")
+    public void addLike(@RequestParam("id") long id){commentS.addLike(id);}
+
+    @GetMapping("/mostPertinentComments")
+    public List<Comment> GetPertinentCommentsBySubject(@RequestParam("id") long id){
+        return commentS.retrieveCommentByPertinence(id); }
+
+    @PutMapping("/pertinentComments")
+    public void updatePertinent(){
+        commentS.setPertinentComments();
+    }
+
+    @GetMapping("/retrieveSubjectComments-idSubject")
+    public List<Comment> retrieveSubjectComments(@RequestParam("id") long id){
+        return commentS.retrieveSubjectComments(id);
+    }
+
+
+}*/
 
 
 
